ecb2064526b3571ce8909d1ed7afaeac2f820e24
gmauros1 <giorgosmauros2000@gmail.com>
2019-12-26 15:26:38 +0200

    δημιουργεία καλύτερου πλέγματος στην κλάση ClassicFrame

--- src/ClassicFrame.java
+++ src/ClassicFrame.java
@@ -2,15 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
 
 public class ClassicFrame /*implements ActionListener */ {
 
     private JFrame frame;
-    //private JButton board[][];
+    private JButton[][] board = new JButton [9][9];
+    private int GAP = 3;
+    private int sqrSide = 3;
+    private int side = 9;
     General classic;
 
     ClassicFrame(){
-        //board = new JButton[9][9];
         classic = new Classic();
         this.makeFrame();
 
@@ -21,19 +24,34 @@ public class ClassicFrame /*implements ActionListener */ {
         frame = new JFrame("Classic Sudoku");
         frame.setLayout(new BorderLayout());
 
-        Container contentPane = frame.getContentPane();
-
-        GridLayout board = new GridLayout(9, 9);
-        contentPane.setLayout(board);
+        Container contentPane1 = frame.getContentPane();
+        contentPane1.setBackground(Color.PINK);
 
 
-        for (int i=0; i<9; i++)
-            for (int j=0; j<9; j++){
-                JButton button = new JButton((i+1)+","+(j+1));
-                contentPane.add(button);
+        GridLayout board1 = new GridLayout(sqrSide, sqrSide, GAP, GAP);
+        contentPane1.setLayout(board1);
 
+        /* ************************* */
+
+        JPanel[][] subpanels = new JPanel[sqrSide][sqrSide];
+
+
+        for (int i=0; i<3; i++)
+            for (int j=0; j<3; j++){
+                subpanels[i][j] = new JPanel(new GridLayout(sqrSide, sqrSide));
+                contentPane1.add(subpanels[i][j]);
             }
 
+
+        for (int i=0; i<side; i++) {
+            int panelIcounter = i / sqrSide;
+            for (int j = 0; j < side; j++) {
+                int panelJcounter = j / sqrSide;
+                board[i][j] = new JButton((1+i)+","+(j+1));
+
+                subpanels[panelIcounter][panelJcounter].add(board[i][j]);
+            }
+        }
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600,600);
